c72df08f4c1acc70c52d39224b6312125f0b0b5f
Hank Bao <hankbao84@gmail.com>
2016-04-17 17:54:12 +0800

    Initial Commit

---
+++ DictObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DictObjc
+//
+//  Created by Hank Bao on 16/4/17.
+//  Copyright © 2016年 zTap Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DictObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictObjc
+//
+//  Created by Hank Bao on 16/4/17.
+//  Copyright © 2016年 zTap Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
